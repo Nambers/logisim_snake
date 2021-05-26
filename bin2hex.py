f0436cd43f77dc389b0beedb6eacf778ba394161
Eritque arcus <1930893235@qq.com>
2021-05-26 22:38:56 +0800

    完善得分机制(不过还有点问题), 修复if goto 逻辑处理，优化指令集

--- bin2hex.py
+++ bin2hex.py
@@ -2,9 +2,14 @@ re = "v2.0 raw\n"
 while True:
     s = input()
     if s == "q":
+        fo = open("x", "w")
+        fo.write(re)
+        fo.close()
+        break
+    if s == "qq":
+        fo = open("x1", "w")
+        fo.write(re)
+        fo.close()
         break
     re += str(hex(int(s,2))).replace("0x", "") + "\n"
-fo = open("x", "w")
-fo.write(re)
-fo.close()
 print(re)
